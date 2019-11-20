33c057d03d78f179cde9389188871300e34070dd
iqbal khan <iqbalhussainkhan@yahoo.com>
2019-11-20 13:00:54 +0400

    Updates

---
+++ precache-manifest.ab8256306bcb86c07677b149f6fcc529.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13d7c292ff9fd924aedcae321b7bbb94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9063950b6def41ef839",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "bfcf27d60345c179e002",
+    "url": "/static/js/2.e28cf578.chunk.js"
+  },
+  {
+    "revision": "d9063950b6def41ef839",
+    "url": "/static/js/main.58030a50.chunk.js"
+  },
+  {
+    "revision": "63fc5d59d94c3ac61187",
+    "url": "/static/js/runtime-main.076be826.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
